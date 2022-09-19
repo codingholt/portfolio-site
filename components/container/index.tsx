90dc66e70b8edfede4fb735b0cfd30d13ac1d3be
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-09-19 17:18:56 +0200

    gradient responsiveness

--- components/container/index.tsx
+++ components/container/index.tsx
@@ -1,11 +1,11 @@
 function Container({ children }: { children: React.ReactNode }) {
 	return (
-		<>
-			<div className="container mx-auto">
-				<span className="min-w-fit flex h-48 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent z-0" />
-				<div className="">{children}</div>
+		<div className="">
+			<div className="container mx-auto relative">
+				<span className="min-w-fit inset-0 absolute h-20 md:h-56 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent z-0" />
+				<div className="relative z-10 top-20">{children}</div>
 			</div>
-		</>
+		</div>
 	);
 }
 
