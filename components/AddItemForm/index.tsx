97dfe66a54c2450c1f6420c3c0cffbd64b4a668e
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-09-29 22:40:12 +0200

    add input to separate file

--- components/AddItemForm/index.tsx
+++ components/AddItemForm/index.tsx
@@ -1,6 +1,7 @@
 import React, { FormEventHandler } from "react";
 import { IProject } from "../../types";
 import { Dispatch, SetStateAction } from "react";
+import { InputItem } from "./input";
 const AddItemForm = ({
 	onSubmit,
 	state,
@@ -20,31 +21,12 @@ const AddItemForm = ({
 					<form onSubmit={onSubmit}>
 						<div className="bg-white dark:bg-black my-4 rounded-lg">
 							<div>
-								<div className="relative bg-inherit my-4">
-									<input
-										type="text"
-										id="naam"
-										name="naam"
-										key={"naam"}
-										className="peer bg-transparent h-10 w-full rounded-lg text-black dark:text-white placeholder-transparent ring-2 px-2 ring-gray-500 focus:ring-sky-600 focus:outline-none focus:border-rose-600"
-										placeholder="Naam"
-										autoComplete="off"
-										value={state?.name}
-										onChange={(e) =>
-											setState({
-												...state,
-												name: e.target.value,
-											})
-										}
-									/>
-									<label
-										htmlFor="naam"
-										className="absolute cursor-text left-0 -top-3 text-sm text-gray-500 bg-inherit mx-1 px-1 
-											peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-500 peer-placeholder-shown:top-2 peer-focus:-top-3 peer-focus:text-sky-600 peer-focus:text-sm transition-all"
-									>
-										Naam.
-									</label>
-								</div>
+								<InputItem
+									placeholder="Naam"
+									valueToUpdate="name"
+									state={state}
+									setState={setState}
+								/>
 							</div>
 							<div className="relative bg-inherit my-4">
 								<button
