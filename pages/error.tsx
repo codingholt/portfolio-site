866e98d25501c689f421f6442c8b4a1e652c701a
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-10-11 14:39:48 +0200

    login error page

---
+++ pages/error.tsx
@@ -0,0 +1,30 @@
+import { Computer } from "iconoir-react";
+import type { NextPage } from "next";
+import Button from "../components/Button";
+import Container from "../components/Container";
+import { useRouter } from "next/router";
+import { NextApiRequest, NextApiResponse } from "next";
+
+const Error: NextPage = () => {
+	const router = useRouter();
+	const { error } = router.query;
+
+	return (
+		<Container color="red">
+			<div className="heading">
+				<p>Sorry :(</p>
+				<p>{error}</p>
+			</div>
+			<div className="inline-flex">
+				<Computer /> <span className="ml-2">Computer says no...</span>
+			</div>
+			<div className="text-gray-500">
+				Sorry er ging iets fout met inloggen probeer het opnieuw door op
+				de knop hieronder te klikken.
+			</div>
+			<Button buttonText="Login" to="/login" extraStyle="mt-2" />
+		</Container>
+	);
+};
+
+export default Error;
