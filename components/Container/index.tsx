2a3d8ee6210d05a8d2ffd9d39046ec2e9059b880
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-10-16 14:19:01 +0200

    fix navbar z-index and containter styling

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -6,7 +6,7 @@ function Container({ children }: { children: React.ReactNode }) {
 			<Navigation />
 			<div className="container mx-auto  relative h-screen">
 				<span className="min-w-fit inset-0 absolute h-20 md:h-56 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent 	z-0" />
-				<div className=" z-10 absolute max-w-full top-1/4">
+				<div className=" z-10 absolute max-w-full md:top-1/4 xl:top-1/4">
 					{children}
 				</div>
 			</div>
--- components/Navigation/index.tsx
+++ components/Navigation/index.tsx
@@ -70,7 +70,7 @@ function Navigation({}: {}) {
 				</div>
 			</div>
 			{/* Mobile */}
-			<div className="xl:hidden container flex justify-end fixed inset-0 z-10">
+			<div className="xl:hidden container flex justify-end fixed inset-0 z-20">
 				{Pages.map((page, idx) => (
 					<div key={idx}>
 						<Link href={page.link}>
