8fb41ac7101ab4c106bdfaa2eed6e13bb42f0f63
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-10-17 14:08:59 +0200

    footer

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -2,18 +2,20 @@ import Navigation from "../Navigation";
 import Footer from "../Footer";
 function Container({ children }: { children: React.ReactNode }) {
 	return (
-		<div className="dark:bg-black h-full dark:text-white duration-1000 overflow-y-auto">
-			<Navigation />
-			<div className="container mx-auto  relative h-screen">
-				<span className="min-w-fit inset-0 absolute h-20 md:h-56 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent 	z-0" />
-				<div className=" z-10 absolute max-w-full md:top-1/4 xl:top-1/4">
-					{children}
+		<>
+			<div className="dark:bg-black h-full dark:text-white duration-1000 overflow-y-auto">
+				<Navigation />
+				<div className="container mx-auto  relative h-screen">
+					<span className="min-w-fit inset-0 absolute h-20 md:h-56 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent 	z-0" />
+					<div className=" z-10 absolute pb-8 max-w-full md:top-1/4 xl:top-1/4">
+						{children}
+					</div>
 				</div>
 			</div>
-			<div className="container mx-auto  relative ">
+			<span className="bottom-0">
 				<Footer />
-			</div>
-		</div>
+			</span>
+		</>
 	);
 }
 
--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -1,43 +1,78 @@
-import { Link } from "iconoir-react";
-import Image from "next/future/image";
+import Link from "next/link";
+import { Pages, Social } from "../../data";
+import { useRouter } from "next/router";
+const ExternalLink = ({
+	href,
+	children,
+}: {
+	href: string;
+	children: React.ReactNode;
+}) => (
+	<a
+		className="text-gray-500 hover:text-gray-600 transition"
+		target="_blank"
+		rel="noopener noreferrer"
+		href={href}
+	>
+		{children}
+	</a>
+);
 
-function Footer() {
-	const year = new Date().getFullYear();
+export default function Footer() {
+	const router = useRouter();
+	const { pathname, asPath, query } = router;
 	return (
-		<footer className="p-4 bg-white rounded-lg shadow md:flex md:items-center md:justify-between md:p-6 dark:bg-gray-800 mb-4">
-			<span className="text-sm text-gray-500 sm:text-center dark:text-gray-400">
-				© {year}{" "}
-				<a href="https://svennijholt.nl" className="hover:underline">
-					Sven Nijholt
-				</a>
-				. All Rights Reserved.
-			</span>
-			<ul className="flex flex-wrap items-center mt-3 text-sm text-gray-500 dark:text-gray-400 sm:mt-0">
-				<li>
-					<a href="#" className="mr-4 hover:underline md:mr-6 ">
-						NL
-					</a>
-				</li>
-				<li>
-					<a href="#" className="mr-4 hover:underline md:mr-6">
-						ENG
-					</a>
-				</li>
+		<footer className="flex flex-col justify-center items-start max-w-2xl mx-auto w-full">
+			<hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
 
-				<li>
-					<a href="#" className="mr-4 hover:underline">
-						Contact
-					</a>
-				</li>
-
-				<li>
-					<a href="#" className="hover:underline">
-						Sitemap
-					</a>
-				</li>
-			</ul>
+			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-4 sm:grid-cols-3">
+				<div className="flex flex-col space-y-4">
+					{Pages.map((page, idx) => {
+						return (
+							<Link href={page.link} key={idx}>
+								<a className="text-gray-500 hover:text-gray-600 transition">
+									{page.name}
+								</a>
+							</Link>
+						);
+					})}
+				</div>
+				<div className="flex flex-col space-y-4">
+					{Social.map((page, idx) => {
+						return (
+							<ExternalLink href={page.link} key={idx}>
+								{page.name}
+							</ExternalLink>
+						);
+					})}
+				</div>
+				<div className="flex flex-col space-y-4">
+					<div
+						className="hover:cursor-pointer"
+						onClick={() =>
+							router.push({ pathname, query }, asPath, {
+								locale: "nl-NL",
+							})
+						}
+					>
+						<a className="text-gray-500 hover:text-gray-600 transition">
+							Dutch
+						</a>
+					</div>
+					<div
+						className="hover:cursor-pointer"
+						onClick={() =>
+							router.push({ pathname, query }, asPath, {
+								locale: "en-US",
+							})
+						}
+					>
+						<a className="text-gray-500 hover:text-gray-600 transition">
+							English
+						</a>
+					</div>
+				</div>
+			</div>
 		</footer>
 	);
 }
-
-export default Footer;
