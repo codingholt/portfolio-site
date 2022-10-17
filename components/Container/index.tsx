9be5f3dcc84e52de71dd1f88a529d691cc193019
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-10-17 14:11:53 +0200

    container responsiveness;

--- components/Container/index.tsx
+++ components/Container/index.tsx
@@ -7,7 +7,7 @@ function Container({ children }: { children: React.ReactNode }) {
 				<Navigation />
 				<div className="container mx-auto  relative h-screen">
 					<span className="min-w-fit inset-0 absolute h-20 md:h-56 bg-gradient-radial-t from-accent-500 via-accent-100/0 to-transparent 	z-0" />
-					<div className=" z-10 absolute pb-8 max-w-full md:top-1/4 xl:top-1/4">
+					<div className=" z-10 absolute pb-8 max-w-full top-[10%] md:top-1/4 xl:top-1/4">
 						{children}
 					</div>
 				</div>
