3051258879dd74d5b26880be4c788c281d158772
holt <75948709+SvenNijholt@users.noreply.github.com>
2022-10-20 11:06:51 +0200

    fix mx footer

--- components/Footer/index.tsx
+++ components/Footer/index.tsx
@@ -22,10 +22,10 @@ export default function Footer() {
 	const router = useRouter();
 	const { pathname, asPath, query } = router;
 	return (
-		<footer className="flex flex-col justify-center items-center max-w-2xl mx-auto w-full">
+		<footer className="flex flex-col justify-center items-center max-w-3xl  mx-auto w-full">
 			<hr className="w-full border-1 border-gray-200 dark:border-gray-800 mb-8" />
 
-			<div className="w-full max-w-2xl grid grid-cols-1 gap-4 pb-4 sm:grid-cols-3">
+			<div className="w-full max-w-3xl grid grid-cols-1 gap-4 pb-4 sm:grid-cols-3">
 				<div className="flex flex-col space-y-4">
 					{Pages.map((page, idx) => {
 						return (
